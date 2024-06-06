2daf8cf43185e1e815e69d3f33e6e26969a27a5d
Poorani Ganesan <pooranig.it@gmail.com>
2024-06-06 06:42:32 +0530

    Test case 4: changed code to get numbers from different delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,10 +1,25 @@
 class StringCalculator
     def add(numbers)
         return 0 if numbers.empty?
-        values = numbers.split(/,|\n|/)
+        p numbers
+        if numbers.start_with?("//")
+            numbers = numbers.gsub("\\n", "\n")
+            delimiter = numbers.scan(/\A\/\/.\s/)[0]
+            numbers_to_sum = numbers.split(delimiter)[1]
+            delimiter = delimiter.gsub("//", "").gsub("\n", "")
+            @values = numbers_to_sum.split(delimiter)
+        else
+            @values = numbers.split(/,|\n|/)
+        end
+
         
-        values.map(&:to_i).sum
+        @values.map(&:to_i).sum
         #to find a char start with // "/;\n23".start_with?("//")
         #to scan delimiters "//;\n23".scan(/\A\/\/.\s/)
+        sum = 0
+        @values.each do |x|
+            sum+=x.to_i
+        end
+        sum
     end
 end
