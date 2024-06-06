c719e62ead3d50724dedae0affb0ac06e5ff22de
Poorani Ganesan <pooranig.it@gmail.com>
2024-06-06 06:19:14 +0530

    Added code in comment to match other delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,9 @@ class StringCalculator
     def add(numbers)
         return 0 if numbers.empty?
         values = numbers.split(/,|\n|/)
-        p values.map(&:to_i)
+        
         values.map(&:to_i).sum
+        #to find a char start with // "/;\n23".start_with?("//")
+        #to scan delimiters "//;\n23".scan(/\A\/\/.\s/)
     end
 end
