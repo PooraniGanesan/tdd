836e78a0d0555e3b7e228e471b880022dd1079d0
Poorani Ganesan <pooranig.it@gmail.com>
2024-06-06 05:33:31 +0530

    Test case2: Sum of positive numbers with delimiter ,

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,5 +2,7 @@ require 'string_calculator'
 RSpec.describe StringCalculator do
     it 'should add numbers from a string' do
       expect(StringCalculator.new.add('')).to eq(0)
+      expect(StringCalculator.new.add('1')).to eq(1)
+      expect(StringCalculator.new.add('1,2')).to eq(3)
     end
 end
