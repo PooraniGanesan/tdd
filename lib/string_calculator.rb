383c1c666829de4be7e3c3db9c23984fa951f17e
Poorani Ganesan <pooranig.it@gmail.com>
2024-06-06 06:58:52 +0530

    Test case 5: negative numbers not allowed exceptin not raised - PASS

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,25 +1,31 @@
 class StringCalculator
     def add(numbers)
         return 0 if numbers.empty?
-        p numbers
+        numbers = numbers.gsub("\\n", "\n")
         if numbers.start_with?("//")
-            numbers = numbers.gsub("\\n", "\n")
             delimiter = numbers.scan(/\A\/\/.\s/)[0]
             numbers_to_sum = numbers.split(delimiter)[1]
             delimiter = delimiter.gsub("//", "").gsub("\n", "")
             @values = numbers_to_sum.split(delimiter)
         else
-            @values = numbers.split(/,|\n|/)
+            @values = numbers.split(/[,\n]/)
         end
 
         
-        @values.map(&:to_i).sum
+        # @values.map(&:to_i).sum
         #to find a char start with // "/;\n23".start_with?("//")
         #to scan delimiters "//;\n23".scan(/\A\/\/.\s/)
+        begin
         sum = 0
         @values.each do |x|
+            p x
+            if x.to_i.negative?
+            raise StandardError.new "negatives not allowed #{x}" 
+            end
             sum+=x.to_i
         end
+        end 
         sum
     end
 end
+
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.new.add('1,2')).to eq(3)
       expect(StringCalculator.new.add('1\n2,3')).to eq(6)
       expect(StringCalculator.new.add('//;\n1;2')).to eq(3)
-      expect {StringCalculator.new.add('//;\n1;2;3-1')}.to raise_error("negatives not allowed -1")
+      expect {StringCalculator.new.add('//;\n1;2;3;-1')}.to raise_error("negatives not allowed -1")
 
     end
 end
