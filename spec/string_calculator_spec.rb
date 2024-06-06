cbe75fcc17e5b481cab4dc83fd604f337127cbc1
Poorani Ganesan <pooranig.it@gmail.com>
2024-06-06 05:36:31 +0530

    Test case 3: Delimiter with \n -Failing

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.new.add('')).to eq(0)
       expect(StringCalculator.new.add('1')).to eq(1)
       expect(StringCalculator.new.add('1,2')).to eq(3)
+      expect(StringCalculator.new.add('1\n2,3')).to eq(6)
     end
 end
